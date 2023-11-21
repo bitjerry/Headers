e921d9508a81a2dc276fd4e9624cb002103dfeeb
Mr.lin <adobe@outlook.my>
2023-11-21 18:30:42 +0800

    perf(Controller): Automatic formatting

--- src/main/java/control/Controller.java
+++ src/main/java/control/Controller.java
@@ -1,17 +1,26 @@
 /**
- *
  * @Time: 2021/12/28 18:38
  * @Author: Mr.lin
  */
 package control;
 
-import com.intellij.openapi.actionSystem.*;
-import com.intellij.openapi.editor.*;
+import com.intellij.codeInsight.actions.ReformatCodeProcessor;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.command.WriteCommandAction;
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.editor.Caret;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.SelectionModel;
 import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiDocumentManager;
+import com.intellij.psi.PsiFile;
+import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 
-import java.awt.Toolkit;
+import java.awt.*;
 import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.Transferable;
@@ -21,19 +30,22 @@ public abstract class Controller extends AnAction {
     private Editor editor;
     private Document document;
 
+    private static final @NonNls Logger LOG = Logger.getInstance(Controller.class);
+
     @Override
     public void update(@NotNull AnActionEvent e) {
         project = e.getRequiredData(CommonDataKeys.PROJECT);
         editor = e.getRequiredData(CommonDataKeys.EDITOR);
         document = editor.getDocument();
         e.getPresentation().setEnabledAndVisible(
-                        project != null &&
+                project != null &&
                         editor != null &&
                         document != null
-                );
+        );
     }
+
     @Override
-    public void actionPerformed(@NotNull AnActionEvent e) {
+    public void actionPerformed(@NotNull AnActionEvent event) {
         Caret primaryCaret = editor.getCaretModel().getPrimaryCaret();
         int start = primaryCaret.getSelectionStart();
         int end = primaryCaret.getSelectionEnd();
@@ -43,26 +55,39 @@ public abstract class Controller extends AnAction {
         } else {
             text = getClipboardText();
         }
-        if (text != null) {
-            String padding = genPadding(primaryCaret.getSelectionStartPosition().column);
-            String result = result(text, padding);
-            writeMessages(start, end, result);
+        if (text == null) {
+            return;
         }
+//        String padding = genPadding(primaryCaret.getSelectionStartPosition().column);
+        try {
+            String result = parse(text).replaceAll("\\r\\n?", "\n");
+            writeMessages(start, end, result);
+        } catch (Exception e) {
+            LOG.error("Oops!!! There was a fucking error in the format-string", e);
+        }
+
     }
 
-    public static String genPadding(int pads){
-        return pads > 0 ? String.format("%"+pads+"s", " ") : "";
-    }
+//    public static String genPadding(int pads){
+//        return pads > 0 ? String.format("%"+pads+"s", " ") : "";
+//    }
 
-    public void writeMessages(int start, int end, String result){
-        WriteCommandAction.runWriteCommandAction(project, () ->{
-            if (result != null){
+    public void writeMessages(int start, int end, String result) {
+        WriteCommandAction.runWriteCommandAction(project, () -> {
+            if (result != null) {
                 document.replaceString(start, end, result);
+                PsiFile psiFile = PsiDocumentManager.getInstance(project).getPsiFile(document);
+                if (psiFile != null) {
+                    SelectionModel selectionModel = editor.getSelectionModel();
+                    selectionModel.setSelection(start, start+result.length());
+                    new ReformatCodeProcessor(psiFile, selectionModel).run();
+                    PsiDocumentManager.getInstance(project).commitDocument(document);
+                }
             }
         });
     }
 
-    public static String getClipboardText(){
+    public static String getClipboardText() {
         String text = null;
         Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
         Transferable contents = clipboard.getContents(null);
@@ -70,7 +95,7 @@ public abstract class Controller extends AnAction {
             try {
                 text = (String) contents.getTransferData(DataFlavor.stringFlavor);
             } catch (Exception e) {
-                e.printStackTrace();
+                LOG.error("Oops!!! There was a fucking error in the Clipboard", e);
             }
         }
         return text;
@@ -79,8 +104,7 @@ public abstract class Controller extends AnAction {
     /**
      * Format text
      * @param text Get test to format
-     * @param padding Set padding characters
      * @return Formatted text
      */
-    public abstract String result(String text, String padding);
+    public abstract String parse(String text) throws Exception;
 }
