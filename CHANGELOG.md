c17cc3382e5759610e9ab796d9af7b3e134d2e1b
Mr.lin <adobe@outlook.my>
2024-02-02 23:07:15 +0800

    style:

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -27,7 +27,7 @@
 - Support browser headers or cookies conversion. Include Google Chrome and Firefox
 - 支持直接从浏览器F12复制的的headers文本与cookies文本格式化为python字典
 
-[Unreleased]: https://github.com/bitjerry/Headers/compare/v2.0.2...HEAD
+[Unreleased]: https://github.com/bitjerry/Headers/compare/v2.0.3...HEAD
 [2.0.2]: https://github.com/bitjerry/Headers/compare/v1.0.4...v2.0.2
 [1.0.4]: https://github.com/bitjerry/Headers/compare/v1.0.3...v1.0.4
 [1.0.3]: https://github.com/bitjerry/Headers/compare/v1.0.2...v1.0.3
--- build.gradle.kts
+++ build.gradle.kts
@@ -61,17 +61,18 @@ tasks {
 
 //         Extract the <!-- Plugin description --> section from README.md and provide for the plugin's manifest
 //        pluginDescription = providers.fileContents(layout.projectDirectory.file("README.md")).asText.map {
-//            val start = "<!-- Plugin description -->"
-//            val end = "<!-- Plugin description end -->"
+////            val start = "<!-- Plugin description -->"
+////            val end = "<!-- Plugin description end -->"
 //
 //            with (it.lines()) {
-//                if (!containsAll(listOf(start, end))) {
-//                    throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
-//                }
-//                subList(indexOf(start) + 1, indexOf(end)).joinToString("\n").let(::markdownToHTML)
+////                if (!containsAll(listOf(start, end))) {
+////                    throw GradleException("Plugin description section not found in README.md:\n$start ... $end")
+////                }
+////                subList(indexOf(start) + 1, indexOf(end)).joinToString("\n").let(::markdownToHTML)
+//                joinToString("\n").let(::markdownToHTML)
 //            }
 //        }
-        pluginDescription = providers.fileContents(layout.projectDirectory.file("DESCRIPTION.md")).asText
+        pluginDescription = providers.fileContents(layout.projectDirectory.file("DESCRIPTION.md")).asText.get().let(::markdownToHTML)
 
         val changelog = project.changelog // local variable for configuration cache compatibility
         // Get the latest available change notes from the changelog file
